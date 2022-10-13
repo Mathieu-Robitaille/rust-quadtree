1059c6227e04d6f3835484801268c6507ab8895e
Mathieu Robitaille <mathieujrobitaille@gmail.com>
2022-10-13 13:07:22 -0400

    cleanup cause i learned about cargo commands

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,35 @@
 mod tree;
 
+use crate::tree::{HasPosition, QuadTree, Rect};
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 use glam::Vec2;
 use rand::Rng;
 use std::fmt::Debug;
-use crate::tree::{ Rect, QuadTree, HasPosition };
-use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
-
 
 const W: f32 = 800.0;
 const H: f32 = 800.0;
 const A: f32 = H * W;
 const DOTS: usize = 1000;
 
-const OFFSET_VEC: Vec2 = Vec2 { x: W / 2.0, y: H / 2.0 };
+const OFFSET_VEC: Vec2 = Vec2 {
+    x: W / 2.0,
+    y: H / 2.0,
+};
 
 fn main() {
     /* QuadTree init */
-
 
     /* Window init */
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
-        .add_plugin(ShapePlugin)        
+        .add_plugin(ShapePlugin)
         .add_startup_system(setup_system)
         .run();
-
 }
 
 fn setup_system(mut commands: Commands) {
-
     let t = quadtree_init();
     let mut bounds: Vec<Rect> = t.get_bounds();
     let points: Vec<Vec2> = t.get_positions();
@@ -38,24 +37,23 @@ fn setup_system(mut commands: Commands) {
     bounds.sort_by(|a, b| a.area().partial_cmp(&b.area()).unwrap());
     bounds.reverse();
 
-    
     for bound in bounds {
         let shape = shapes::Rectangle {
             extents: bound.size,
-            origin: RectangleOrigin::BottomLeft
+            origin: RectangleOrigin::BottomLeft,
         };
 
         // let norm = normalize_bound(bound.area());
-        let transform_vec = Vec3 { 
+        let transform_vec = Vec3 {
             x: bound.pos.x - OFFSET_VEC.x,
             y: bound.pos.y - OFFSET_VEC.y,
-            z: 0.0
+            z: 0.0,
         };
 
         commands.spawn_bundle(GeometryBuilder::build_as(
             &shape,
-            DrawMode::Outlined { 
-                fill_mode: FillMode::color(Color::BLACK), 
+            DrawMode::Outlined {
+                fill_mode: FillMode::color(Color::BLACK),
                 outline_mode: StrokeMode::new(Color::WHITE, 1.0),
             },
             Transform::from_translation(transform_vec),
@@ -64,23 +62,31 @@ fn setup_system(mut commands: Commands) {
 
     for spot in points {
         let shape = shapes::Circle {
-            center: spot - OFFSET_VEC, radius: 1.0
+            center: spot - OFFSET_VEC,
+            radius: 1.0,
         };
 
         commands.spawn_bundle(GeometryBuilder::build_as(
-            &shape, 
-            DrawMode::Stroke(StrokeMode { options: StrokeOptions::default(), color: Color::RED }), 
+            &shape,
+            DrawMode::Stroke(StrokeMode {
+                options: StrokeOptions::default(),
+                color: Color::RED,
+            }),
             Transform::default(),
         ));
     }
 
     let shape = shapes::Circle {
-        center: Vec2 { x: 0.0, y: 0.0 }, radius: 2.0
+        center: Vec2 { x: 0.0, y: 0.0 },
+        radius: 2.0,
     };
 
     commands.spawn_bundle(GeometryBuilder::build_as(
-        &shape, 
-        DrawMode::Stroke(StrokeMode { options: StrokeOptions::default(), color: Color::GREEN }), 
+        &shape,
+        DrawMode::Stroke(StrokeMode {
+            options: StrokeOptions::default(),
+            color: Color::GREEN,
+        }),
         Transform::default(),
     ));
 
@@ -88,11 +94,14 @@ fn setup_system(mut commands: Commands) {
 }
 
 fn quadtree_init() -> QuadTree<Vec2> {
-    let x: Rect = Rect{ pos: Vec2{ x: 0.0, y: 0.0 }, size: Vec2 { x: W, y: H } };
+    let x: Rect = Rect {
+        pos: Vec2 { x: 0.0, y: 0.0 },
+        size: Vec2 { x: W, y: H },
+    };
     let mut t: QuadTree<Vec2> = QuadTree::new_empty(x);
 
     let mut rng = rand::thread_rng();
-    
+
     /* generate DOTS random points */
     for _ in 1..DOTS {
         let (x, y) = (rng.gen_range(0.0..W), rng.gen_range(0.0..H));
@@ -101,11 +110,10 @@ fn quadtree_init() -> QuadTree<Vec2> {
     t
 }
 
-
 #[allow(unused)]
 fn redraw<T>(_t: QuadTree<T>)
 where
-    T: HasPosition + Copy + Debug
+    T: HasPosition + Copy + Debug,
 {
     unimplemented!();
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,34 +1,41 @@
-use glam::{Vec2, Mat2};
+use glam::{Mat2, Vec2};
 
-use std::fmt::Debug;
 use bevy::prelude::Component;
+use std::fmt::Debug;
 
 const CAPACITY: usize = 4;
 
 // https://www.topcoder.com/thrive/articles/Geometry%20Concepts%20part%202:%20%20Line%20Intersection%20and%20its%20Applications
 
-
-pub struct QuadTree<T> 
+pub struct QuadTree<T>
 where
-    T: HasPosition + Copy + Debug
+    T: HasPosition + Copy + Debug,
 {
     pub bounds: Rect,
     pub points: Option<Vec<T>>,
-    pub inner_trees: Option<Box::<[QuadTree<T>; 4]>>
+    pub inner_trees: Option<Box<[QuadTree<T>; 4]>>,
 }
 
-impl<T> QuadTree<T> 
+impl<T> QuadTree<T>
 where
-    T: HasPosition + Copy + Debug
+    T: HasPosition + Copy + Debug,
 {
     #[allow(unused)]
     pub fn new_empty(r: Rect) -> QuadTree<T> {
-        QuadTree { bounds: r, points: None, inner_trees: None }
+        QuadTree {
+            bounds: r,
+            points: None,
+            inner_trees: None,
+        }
     }
 
     #[allow(unused)]
     pub fn from(i: Vec<T>, r: Rect) -> QuadTree<T> {
-        let mut q: QuadTree<T> = QuadTree{ bounds: r, points: None, inner_trees: None };
+        let mut q: QuadTree<T> = QuadTree {
+            bounds: r,
+            points: None,
+            inner_trees: None,
+        };
         for obj in i {
             q.insert(obj);
         }
@@ -39,7 +46,9 @@ where
     // return false if it cannot be inserted for any reason
     #[allow(unused)]
     pub fn insert(&mut self, p: T) -> bool {
-        if !self.bounds.contains(&p) { return false; }
+        if !self.bounds.contains(&p) {
+            return false;
+        }
         match (self.points.as_mut(), self.inner_trees.as_mut()) {
             // this tree itself contains points
             (Some(val), None) => {
@@ -55,9 +64,7 @@ where
             // This tree contains subtrees but no points
             (None, Some(val)) => {
                 let x = &mut **val;
-                return x
-                    .iter_mut()
-                    .any(|x| x.insert(p))
+                return x.iter_mut().any(|x| x.insert(p));
             }
             // This tree has nothing cool about it
             (None, None) => {
@@ -70,7 +77,6 @@ where
         }
     }
 
-
     // Returns a copy of each elements position
     // This is intended to be a read-only func
     #[allow(unused)]
@@ -81,11 +87,9 @@ where
             // this tree itself contains points
             // get all the points and send them back up
             (Some(val), None) => {
-                let mut x: Vec<Vec2> = val.iter()
-                    .map(|x| x.get_pos())
-                    .collect();
+                let mut x: Vec<Vec2> = val.iter().map(|x| x.get_pos()).collect();
                 res.append(&mut x);
-            },
+            }
 
             // This tree contains subtrees but no points
             (None, Some(val)) => {
@@ -94,8 +98,8 @@ where
                     let mut cont = i.get_positions();
                     res.append(&mut cont);
                 }
-            },
-            _ => ()
+            }
+            _ => (),
         }
         res
     }
@@ -111,6 +115,8 @@ where
     #[allow(unused)]
     fn get_bounds_internal(&self) -> Vec<Rect> {
         let mut result: Vec<Rect> = vec![self.bounds];
+
+        // Unbox the trees so we can use them
         if let Some(boxed_inner_trees) = &self.inner_trees {
             let inner_trees = &**boxed_inner_trees;
             for x in inner_trees.iter() {
@@ -130,10 +136,9 @@ where
         if let Some(boxed_inner_trees) = &self.inner_trees {
             let inner_trees = &**boxed_inner_trees;
             for x in inner_trees.iter() {
-
                 // For the bounds we intersect recur this function
                 if let Some(_) = l.linerect_intersect(x.bounds) {
-                    res.append(&mut QuadTree::get_intersecting_bounds(&x, l));
+                    res.append(&mut QuadTree::get_intersecting_bounds(x, l));
                 }
             }
         }
@@ -146,19 +151,15 @@ where
     #[allow(unused)]
     pub fn get_objects(&self) -> Vec<T> {
         let res: Vec<T> = vec![];
-        
+
         match (self.points.as_ref(), self.inner_trees.as_ref()) {
             // this tree itself contains points
             // get all the points and send them back up
-            (Some(val), None) => {
-                
-            },
+            (Some(val), None) => {}
 
             // This tree contains subtrees but no points
-            (None, Some(val)) => {
-
-            },
-            _ => ()
+            (None, Some(val)) => {}
+            _ => (),
         }
         res
     }
@@ -175,64 +176,87 @@ where
 
         /*
 
-      (0, 0)
-        |
-        V
-         _______ _______
-        |       |       |
-        |   2   |   1   |
-        |_______|_______|
-        |       |       |
-        |   3   |   4   |
-        |_______|_______|
+        (0, 0)
+          |
+          V
+           _______ _______
+          |       |       |
+          |   2   |   1   |
+          |_______|_______|
+          |       |       |
+          |   3   |   4   |
+          |_______|_______|
 
-        */
+          */
 
+        let r1 = Rect {
+            pos: Vec2 {
+                x: self.bounds.pos.x + half_w,
+                y: self.bounds.pos.y,
+            },
+            size: Vec2 {
+                x: half_w,
+                y: half_h,
+            },
+        };
+        let r2 = Rect {
+            pos: Vec2 {
+                x: self.bounds.pos.x,
+                y: self.bounds.pos.y,
+            },
+            size: Vec2 {
+                x: half_w,
+                y: half_h,
+            },
+        };
+        let r3 = Rect {
+            pos: Vec2 {
+                x: self.bounds.pos.x,
+                y: self.bounds.pos.y + half_h,
+            },
+            size: Vec2 {
+                x: half_w,
+                y: half_h,
+            },
+        };
+        let r4 = Rect {
+            pos: Vec2 {
+                x: self.bounds.pos.x + half_w,
+                y: self.bounds.pos.y + half_w,
+            },
+            size: Vec2 {
+                x: half_w,
+                y: half_h,
+            },
+        };
 
-        let r1 = Rect { 
-            pos: Vec2 {x: self.bounds.pos.x + half_w, y: self.bounds.pos.y },
-            size: Vec2 {x: half_w, y: half_h } 
-        };
-        let r2 = Rect { 
-            pos: Vec2 { x: self.bounds.pos.x, y: self.bounds.pos.y }, 
-            size: Vec2 { x: half_w, y: half_h }
-        };
-        let r3 = Rect { 
-            pos: Vec2 { x: self.bounds.pos.x, y: self.bounds.pos.y + half_h }, 
-            size: Vec2 { x: half_w, y: half_h}
-        };
-        let r4 = Rect { 
-            pos: Vec2 { x: self.bounds.pos.x + half_w, y: self.bounds.pos.y + half_w }, 
-            size: Vec2 { x: half_w, y: half_h }
-        };
-
-        let ret: [QuadTree<T>; 4] = [QuadTree {
-            bounds: r1,
-            points: Some(r1.points_inside(&self.points)),
-            inner_trees: None,
-        }, 
-        QuadTree {
-            bounds: r2,
-            points: Some(r2.points_inside(&self.points)),
-            inner_trees: None,
-        }, 
-        QuadTree {
-            bounds: r3,
-            points: Some(r3.points_inside(&self.points)),
-            inner_trees: None,
-        },
-        QuadTree {
-            bounds: r4,
-            points: Some(r4.points_inside(&self.points)),
-            inner_trees: None,
-        }];
+        let ret: [QuadTree<T>; 4] = [
+            QuadTree {
+                bounds: r1,
+                points: Some(r1.points_inside(&self.points)),
+                inner_trees: None,
+            },
+            QuadTree {
+                bounds: r2,
+                points: Some(r2.points_inside(&self.points)),
+                inner_trees: None,
+            },
+            QuadTree {
+                bounds: r3,
+                points: Some(r3.points_inside(&self.points)),
+                inner_trees: None,
+            },
+            QuadTree {
+                bounds: r4,
+                points: Some(r4.points_inside(&self.points)),
+                inner_trees: None,
+            },
+        ];
 
         self.inner_trees = Some(Box::new(ret));
         self.points = None;
     }
-
 }
-
 
 pub trait HasPosition {
     fn get_pos(&self) -> Vec2;
@@ -246,7 +270,11 @@ pub struct Rect {
 
 impl Rect {
     pub fn contains<T: HasPosition>(&self, v: &T) -> bool {
-        if v.get_pos().x >= self.pos.x && v.get_pos().x < self.pos.x + self.size.x && v.get_pos().y >= self.pos.y && v.get_pos().y < self.pos.y + self.size.y {
+        if v.get_pos().x >= self.pos.x
+            && v.get_pos().x < self.pos.x + self.size.x
+            && v.get_pos().y >= self.pos.y
+            && v.get_pos().y < self.pos.y + self.size.y
+        {
             return true;
         }
         false
@@ -256,19 +284,19 @@ impl Rect {
         self.size.x * self.size.y
     }
     // Return the points in the specified rectangle
-    pub fn points_inside<T>(&self, points: &Option<Vec<T>>) -> Vec<T> 
+    pub fn points_inside<T>(&self, points: &Option<Vec<T>>) -> Vec<T>
     where
-        T: HasPosition + Copy
+        T: HasPosition + Copy,
     {
         if let Some(p) = points {
-            return p.iter()
+            return p
+                .iter()
                 .filter(|&x| self.contains(x))
                 .copied()
-                .collect::<Vec<T>>()
-        } else {
-            return vec![];
+                .collect::<Vec<T>>();
         }
-    }  
+        return vec![];
+    }
 }
 
 impl HasPosition for Rect {
@@ -279,7 +307,10 @@ impl HasPosition for Rect {
 
 impl HasPosition for Vec2 {
     fn get_pos(&self) -> Vec2 {
-        Vec2 { x: self.x, y: self.y }
+        Vec2 {
+            x: self.x,
+            y: self.y,
+        }
     }
 }
 
@@ -289,12 +320,13 @@ pub struct Line {
     end: Vec2,
 }
 
+#[allow(unused)]
 impl Line {
     pub fn new(x1: f32, y1: f32, x2: f32, y2: f32) -> Line {
-        Line { 
-            origin: Vec2 { x: x1, y: y1 }, 
+        Line {
+            origin: Vec2 { x: x1, y: y1 },
             end: Vec2 { x: x2, y: y2 },
-         }
+        }
     }
 
     // Ax + Bx = C
@@ -309,7 +341,10 @@ impl Line {
         self.a() + self.b()
     }
     fn ab(&self) -> Vec2 {
-        Vec2 { x: self.a(), y: self.b() }
+        Vec2 {
+            x: self.a(),
+            y: self.b(),
+        }
     }
 
     fn ua(&self, l2: &Line) -> f32 {
@@ -319,19 +354,20 @@ impl Line {
     }
 
     fn ub(&self, l2: &Line) -> f32 {
-        let d1: f32 = Mat2::from_cols(self.end - self.origin, self.origin - l2.origin).determinant();
+        let d1: f32 =
+            Mat2::from_cols(self.end - self.origin, self.origin - l2.origin).determinant();
         let d2: f32 = Mat2::from_cols(self.end - self.origin, l2.end - l2.origin).determinant();
         d1 / d2
     }
 
     fn lineline_intersect(&self, l2: &Line) -> Option<Vec2> {
-        let ua = self.ua(&l2);
-        let ub = self.ub(&l2);
+        let ua = self.ua(l2);
+        let ub = self.ub(l2);
         // lines are colliding if ua and ub are within [0, 1]
-        if ua >= 0.0 && ua <= 1.0 && ub >= 0.0 && ub <= 1.0 {
-            return Some(Vec2{
-                x: self.origin.x + (ua * self.b()),
-                y: self.origin.y + (ua * self.a())
+        if (0.0..=1.0).contains(&ua) && (0.0..=1.0).contains(&ub) {
+            return Some(Vec2 {
+                x: self.origin.x + (ub * self.b()),
+                y: self.origin.y + (ub * self.a()),
             });
         }
         None
@@ -339,16 +375,79 @@ impl Line {
 
     #[allow(unused)]
     fn linerect_intersect(&self, r: Rect) -> Option<Vec<Vec2>> {
-        let left: Option<Vec2> = self.lineline_intersect(
-            &Line::new(r.pos.x, r.pos.y, r.pos.x, r.pos.y + r.size.y));
-        let right: Option<Vec2> = self.lineline_intersect(
-            &Line::new(r.pos.x + r.size.x, r.pos.y, r.pos.x, r.pos.y + r.size.y));
-        let top: Option<Vec2> = self.lineline_intersect(
-            &Line::new(r.pos.x, r.pos.y, r.pos.x + r.size.x, r.pos.y));
-        let bottom: Option<Vec2> = self.lineline_intersect(
-            &Line::new(r.pos.x, r.pos.y + r.size.y, r.pos.x + r.size.x, r.pos.y + r.size.y));
+        let left: Option<Vec2> =
+            self.lineline_intersect(&Line::new(r.pos.x, r.pos.y, r.pos.x, r.pos.y + r.size.y));
+        let right: Option<Vec2> = self.lineline_intersect(&Line::new(
+            r.pos.x + r.size.x,
+            r.pos.y,
+            r.pos.x,
+            r.pos.y + r.size.y,
+        ));
+        let top: Option<Vec2> =
+            self.lineline_intersect(&Line::new(r.pos.x, r.pos.y, r.pos.x + r.size.x, r.pos.y));
+        let bottom: Option<Vec2> = self.lineline_intersect(&Line::new(
+            r.pos.x,
+            r.pos.y + r.size.y,
+            r.pos.x + r.size.x,
+            r.pos.y + r.size.y,
+        ));
         todo!();
         None
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lineline_intersect_1() {
+        let l1 = Line {
+            origin: Vec2 { x: 0.0, y: 0.0 },
+            end: Vec2 { x: 10.0, y: 10.0 },
+        };
+        let l2 = Line {
+            origin: Vec2 { x: 0.0, y: 10.0 },
+            end: Vec2 { x: 10.0, y: 0.0 },
+        };
+        assert_eq!(
+            Line::lineline_intersect(&l1, &l2).unwrap(),
+            Vec2 { x: 5.0, y: 5.0 }
+        );
+    }
+
+    #[test]
+    fn lineline_intersect_2() {
+        let l1 = Line {
+            origin: Vec2 { x: 10.0, y: 10.0 },
+            end: Vec2 { x: 20.0, y: 20.0 },
+        };
+        let l2 = Line {
+            origin: Vec2 { x: 10.0, y: 20.0 },
+            end: Vec2 { x: 20.0, y: 10.0 },
+        };
+        assert_eq!(
+            Line::lineline_intersect(&l1, &l2).unwrap(),
+            Vec2 { x: 15.0, y: 15.0 }
+        );
+    }
+
+    #[test]
+    fn lineline_intersect_negative_1() {
+        let l1 = Line {
+            origin: Vec2 { x: -10.0, y: 10.0 },
+            end: Vec2 { x: 10.0, y: -10.0 },
+        };
+        let l2 = Line {
+            origin: Vec2 { x: -15.0, y: -15.0 },
+            end: Vec2 { x: 10.0, y: 15.0 },
+        };
+        assert_eq!(
+            Line::lineline_intersect(&l1, &l2).unwrap(),
+            Vec2 {
+                x: -1.364,
+                y: 1.364
+            }
+        );
+    }
+}
