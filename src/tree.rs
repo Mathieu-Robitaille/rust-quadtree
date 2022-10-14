e8dc68e98d063d19ec841108d05079b063d8dafa
Mathieu Robitaille <mathieujrobitaille@gmail.com>
2022-10-13 20:00:57 -0400

    Added tests and fixed ua

--- src/tree.rs
+++ src/tree.rs
@@ -1,8 +1,6 @@
-use glam::{Mat2, Vec2};
-
 use bevy::prelude::Component;
+use glam::{Mat2, Vec2};
 use std::fmt::Debug;
-
 const CAPACITY: usize = 4;
 
 // https://www.topcoder.com/thrive/articles/Geometry%20Concepts%20part%202:%20%20Line%20Intersection%20and%20its%20Applications
@@ -140,7 +138,7 @@ where
             let inner_trees = &**boxed_inner_trees;
             for x in inner_trees.iter() {
                 // For the bounds we intersect recur this function
-                if l.linerect_intersect(x.bounds).is_some() {
+                if l.linerect_intersect(&x.bounds).is_ok() {
                     res.append(&mut QuadTree::get_intersecting_bounds(x, l));
                 }
             }
@@ -353,55 +351,77 @@ impl Line {
     fn ua(&self, l2: &Line) -> f32 {
         let d1: f32 = Mat2::from_cols(l2.end - l2.origin, self.origin - l2.origin).determinant();
         let d2: f32 = Mat2::from_cols(l2.end - l2.origin, self.end - self.origin).determinant();
-        -(d1 / d2)
+        -d1 / d2
+    }
+
+    fn ua2(&self, l2: &Line) -> f32 {
+        let (x1, y1) = (self.origin.x, self.origin.y);
+        let (x2, y2) = (self.end.x, self.end.y);
+        let (x3, y3) = (l2.origin.x, l2.origin.y);
+        let (x4, y4) = (l2.end.x, l2.end.y);
+
+        let d1: f32 = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3))
+            / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+        // println!("{:?}", d1);
+        d1
     }
 
     fn ub(&self, l2: &Line) -> f32 {
         let d1: f32 =
             Mat2::from_cols(self.end - self.origin, self.origin - l2.origin).determinant();
         let d2: f32 = Mat2::from_cols(l2.end - l2.origin, self.end - self.origin).determinant();
-        d1 / d2
+        -d1 / d2
     }
 
-    fn lineline_intersect(&self, l2: &Line) -> Option<Vec2> {
+    fn lineline_intersect(&self, l2: &Line) -> Result<Vec2, &str> {
         let ua = self.ua(l2);
         let ub = self.ub(l2);
+        println!("{:?} {:?}", ua, ub);
         // lines are colliding if ua and ub are within [0, 1]
         if (0.0..=1.0).contains(&ua) && (0.0..=1.0).contains(&ub) {
-            return Some(Vec2 {
+            let intersection_point = Vec2 {
                 x: self.origin.x + (ua * self.b()),
                 y: self.origin.y + (ua * self.a()),
-            });
+            };
+            return Ok(intersection_point);
         }
-        None
+        Err("No intersection.")
     }
 
     #[allow(unused)]
-    fn linerect_intersect(&self, r: Rect) -> Option<Vec<Vec2>> {
-        let left: Option<Vec2> =
+    fn linerect_intersect(&self, r: &Rect) -> Result<Vec<Vec2>, &str> {
+        let left: Result<Vec2, &str> =
             self.lineline_intersect(&Line::new(r.pos.x, r.pos.y, r.pos.x, r.pos.y + r.size.y));
-        let right: Option<Vec2> = self.lineline_intersect(&Line::new(
+        let right: Result<Vec2, &str> = self.lineline_intersect(&Line::new(
             r.pos.x + r.size.x,
             r.pos.y,
             r.pos.x,
             r.pos.y + r.size.y,
         ));
-        let top: Option<Vec2> =
+        let top: Result<Vec2, &str> =
             self.lineline_intersect(&Line::new(r.pos.x, r.pos.y, r.pos.x + r.size.x, r.pos.y));
-        let bottom: Option<Vec2> = self.lineline_intersect(&Line::new(
+        let bottom: Result<Vec2, &str> = self.lineline_intersect(&Line::new(
             r.pos.x,
             r.pos.y + r.size.y,
             r.pos.x + r.size.x,
             r.pos.y + r.size.y,
         ));
-        todo!();
-        None
+        let collection = [left, right, top, bottom];
+        if collection.iter().any(|x| x.is_ok()) {
+            return Ok(collection
+                .iter()
+                .filter(|&&x| x.is_ok())
+                .map(|&x| x.unwrap())
+                .collect::<Vec<Vec2>>());
+        }
+        Err("No collisions.")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn lineline_intersect_1() {
@@ -420,6 +440,39 @@ mod tests {
     }
 
     #[test]
+    fn ua_test_rng() {
+        let mut rng = rand::thread_rng();
+        for _ in 1..100 {
+            let (x1, y1) = (rng.gen_range(1.0..10.0), rng.gen_range(1.0..10.0));
+            let (x2, y2) = (rng.gen_range(1.0..10.0), rng.gen_range(1.0..10.0));
+            let (x3, y3) = (rng.gen_range(1.0..10.0), rng.gen_range(1.0..10.0));
+            let (x4, y4) = (rng.gen_range(1.0..10.0), rng.gen_range(1.0..10.0));
+            let l1: Line = Line {
+                origin: Vec2 { x: x1, y: y1 },
+                end: Vec2 { x: x2, y: y2 },
+            };
+            let l2: Line = Line {
+                origin: Vec2 { x: x3, y: y3 },
+                end: Vec2 { x: x4, y: y4 },
+            };
+            assert_eq!(Line::ua(&l1, &l2), Line::ua2(&l1, &l2))
+        }
+    }
+
+    #[test]
+    fn ua_test_1() {
+        let l1 = Line {
+            origin: Vec2 { x: 0.0, y: 0.0 },
+            end: Vec2 { x: 10.0, y: 10.0 },
+        };
+        let l2 = Line {
+            origin: Vec2 { x: 0.0, y: 10.0 },
+            end: Vec2 { x: 10.0, y: 0.0 },
+        };
+        assert_eq!(Line::ua(&l1, &l2), Line::ua2(&l1, &l2))
+    }
+
+    #[test]
     fn lineline_intersect_2() {
         let l1 = Line {
             origin: Vec2 { x: 10.0, y: 10.0 },
@@ -433,6 +486,15 @@ mod tests {
             Line::lineline_intersect(&l1, &l2).unwrap(),
             Vec2 { x: 15.0, y: 15.0 }
         );
+    }
+
+    #[test]
+    fn lineline_intersect_overlap_1() {
+        let l1 = Line {
+            origin: Vec2 { x: 10.0, y: 10.0 },
+            end: Vec2 { x: 20.0, y: 20.0 },
+        };
+        assert_eq!(Line::lineline_intersect(&l1, &l1), Err("No intersection."));
     }
 
     #[test]
@@ -454,4 +516,16 @@ mod tests {
         );
     }
 
+    // #[test]
+    // fn linerect_intersect_1() {
+    //     let l: Line = Line {
+    //         origin: Vec2 { x: 0.0, y: 0.0 },
+    //         end: Vec2 { x: 10.0, y: 10.0 },
+    //     };
+    //     let r: Rect = Rect {
+    //         pos: Vec2 { x: 4.1, y: 6.1 },
+    //         size: Vec2 { x: 2.0, y: 2.0 },
+    //     };
+    //     assert_ne!(Line::linerect_intersect(&l, &r).unwrap(), RectIntersect {});
+    // }
 }
